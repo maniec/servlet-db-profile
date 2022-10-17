0b189af3cdffa9e69be30fce0a916abf44b7f067
gschipper <gschipper@gmail.com>
2022-10-17 20:31:05 +0200

    pojo metadata

--- src/main/java/com/maniec/Person.java
+++ src/main/java/com/maniec/Person.java
@@ -1,13 +1,29 @@
 package com.maniec;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Person {
 
+    @Id
+    @GeneratedValue
+    private Long id;
     private String name;
     private String favoriteFruit;
 
     public Person(String name, String favoriteFruit) {
         this.name = name;
         this.favoriteFruit = favoriteFruit;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
